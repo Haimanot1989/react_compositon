f1e11a93c974778eaf2d05b2c49a8a003ffaea00
haitekie <haimanot.tekie@finn.no>
2021-08-30 12:06:33 +0200

    setup initial example for Using Composition in React to Avoid 'Prop Drilling'

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react_compositon
+
+Walking through the example regarding how to Using Composition in React to Avoid "Prop Drilling"
+
+https://www.youtube.com/watch?v=3XaXKiXtNjw
--- src/App.js
+++ src/App.js
@@ -1,5 +1,84 @@
+import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Hello World</div>;
+  let [currentUser, setCurrentUser] = React.useState(null);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      <div style={{ backgroundColor: "lightgray" }}>
+        <Header />
+      </div>
+      <div style={{ flex: 1 }}>
+        {currentUser ? (
+          <Dashboard user={currentUser} />
+        ) : (
+          <LoginScreen onLogin={() => setCurrentUser({ name: "Haimanot" })} />
+        )}
+      </div>
+      <div style={{ backgroundColor: "lightgray" }}>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+}
+
+function LoginScreen({ onLogin }) {
+  return (
+    <div>
+      <h3>Please Login</h3>
+      <button onClick={onLogin}>Login</button>
+    </div>
+  );
+}
+
+function Dashboard({ user }) {
+  return (
+    <div>
+      <h2>The Dashboard</h2>
+      <DashboardNav />
+      <DashboardContent user={user} />
+    </div>
+  );
+}
+
+function DashboardNav() {
+  return (
+    <div>
+      <h3>Dashboard Nav</h3>
+    </div>
+  );
+}
+
+function DashboardContent({ user }) {
+  return (
+    <div>
+      <h3>Dashboard Content</h3>
+      <WelcomeMessage user={user} />
+    </div>
+  );
+}
+
+function WelcomeMessage({ user }) {
+  return (
+    <div>
+      <p>Welcome {user.name}!</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
 }
